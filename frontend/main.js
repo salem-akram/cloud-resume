f882b5183be39d154d4367a854de0e7f500efd02
salem-akram <Salem_8870>
2024-11-17 07:57:59 +0300

    deployed storage and Azure function

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event)=>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl ='https://getresumecountervalue.azurewebsites.net/api/GetResumeCounter?code=ilJf3oY_YHlqdlaBaEB_WeTTdCH-uOe4Tpf-jBJCphXsAzFuXdzMow%3D%3D';
+const LocalFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count =30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
